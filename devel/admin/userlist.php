9d4c26054d21135ca6f210e2189799bf3047d4b7
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2004-12-14 18:51:04 +0000

    Updated admin/userlist.php and hopefully got rid of the errors in Linux. All project members using a OS filled with errors (you know who you are), please remember to use a texteditor that supports UNIX linebreaks :)

--- devel/admin/userlist.php
+++ devel/admin/userlist.php
@@ -1,68 +1,42 @@
 <?php
-
 require 'config/config.php';
-
 
 $action = $_GET['action'];
 
 if($action == "display" || !isset($action)) {
 	if(!acl_access("adminUsers")) die($admin[noaccess]);
 	echo "<table>";
-
 	$query = mysql_query("SELECT * FROM users WHERE ID != 1 ORDER BY ID ASC") or die(mysql_error());
 
-
-
 	$num = mysql_num_rows($query);
-
 	echo "<tr><th>$profile[8]</th>";
-
 	echo "</tr>";
-
 	for($i=0;$i<$num;$i++) {
 
 		$row = mysql_fetch_object($query);
 
-
-
 		echo "<tr><td><a href=index.php?inc=useradmin&user=$row->ID>$row->nick</a></td>";
-
 		echo "</tr>";
 
-
-
 	}
-
 	echo "</table>";
-
 }
 
 elseif($action == "online") {
 	if(!acl_access("onlineUsers")) die($admin[noaccess]);
 
 	echo "<table>";
-
 	echo "<tr><th>$profile[8]</th><th>IP</th><th>Last page-view</th><th>Side</th></tr>";
-
 	$query = mysql_query("SELECT * FROM session ORDER BY userID") or die(mysql_error());
-
 	$num = mysql_num_rows($query);
-
-
 
 	for($i=0;$i<$num;$i++) {
 
 		$row = mysql_fetch_object($query);
 
-
-
 		$nick = mysql_query("SELECT nick FROM users WHERE ID = $row->userID");
-
 		$nick = mysql_fetch_row($nick);
-
 		$nick = $nick[0];
-
-
 
 		echo "<tr><td>$nick</td><td>$row->IP</td><td>";
 		echo date("H:i",$row->logUNIX)."";
