fe171a3130f8df99ff3da868ef79593b34e5ff5f
Mathias Bøhn Grytemark <mboehn(at)github.com>
2004-12-22 16:57:58 +0000

    Cleaned up config/config.php a bit. I have not yet removed any old parts from it. Will do that in a moment.

--- devel/config/config.php
+++ devel/config/config.php
@@ -1,64 +1,212 @@
 <?php
 
-/* This one will define where the root-path of all your files are.... */
-#$base_path = "/var/www/devel/"; // Manually specified path
-$base_path = $_SERVER['DOCUMENT_ROOT']."/";
+/*
+ * OSGlobeLAN configuration file
+ *
+ * http://www.sourceforge.net/projects/osglobelan
+ *
+ * Read docs/README first.
+ */
 
-/* Site-title will show up as the <title></title> text on all the pages.... */
-$sitetitle = "GlobeLAN 4,5 DEVEL";
-$devel_version = "0.3 Alpha";
 
-/* config for the MySQL server! */
+/*
+ * base_path
+ * 
+ * The path to the installation of OSGlobeLAN.
+ * Default is the document root of the webserver config or virtualhost.
+ *
+ */
+//$base_path = "/var/www/devel/";
+$base_bath = $_SERVER['DOCUMENT_ROOT']."/";
+
+
+/*
+ * sitetitle
+ *
+ * Title on all pages.
+ */
+$sitetitle = "OSGlobeLAN";
+
+
+/*
+ * devel_version
+ *
+ * Sets current version of OSGlobeLAN.
+ */
+$devel_version = "CVS";
+
+
+/*
+ * sql_server
+ *
+ * Hostname or IP-address to the MySQL-server.
+ */
 $sql_server = "localhost";
+
+
+/*
+ * sql_user
+ *
+ * Username for the MySQL-server.
+ */
 $sql_user = "devel";
+
+
+/*
+ * sql_password
+ *
+ * Password for the MySQL-server.
+ */
 $sql_pass = "devel";
+
+
+/*
+ * sql_db
+ *
+ * Name of the database to use.
+ */
 $sql_db = "devel";
 
-/* Setting this to upgrade will automatically upgrade the database (hopefully) when an admin is logged in.... */
-/* Should probably be disabled when the upgrade is completed */
-#$sql_mode = "upgrade";
 
-/* This file contains the $sql_* variables shown above */
-#include_once '/home/lak/inc_devel.php';
-
-/* config for database handling */
-$session_alive_time = 3600;      // How long should a session stay alive? 3600 (one hour) is default.
-$cookiename = "GL-D3V3L";
-/* The language-files to use.... If your language is not in the lang-folder, make it, translate it, and send it to the creators... */
-$language = "english";
-
-/* Which systems to use... */
+/*
+ * session_alive_time
+ *
+ * How long does a session last?
+ */
+$session_alive_time = "3600";
 
 
-$usenews = 3;			// Number of news-items to show on first-page *FALSE to disable*
-$userand = TRUE;		// Enable random-system, TRUE to give random from the table random on every page-view... random_quote() to put it in on the page.... (default used in bottom.php)
+/*
+ * cookiename
+ *
+ * Name of the cookies.
+ */
+//$cookiename = "Maryland_Cookies";
+$cookiename = "OSGlobeLAN";
 
-$usestats = TRUE;
+/*
+ * language
+ *
+ * Systemwide language to use.
+ * Current choices is "english" and "norwegian".
+ */
+ $language = "english";
 
+
+ /*
+  * usenews
+  *
+  * How many newsitems to show on front page.
+  * Set to FALSE to disable news on the front page.
+  */
+$usenews = 3;
+
+
+/*
+ * userand
+ *
+ * Enable the random-quote-system.
+ * Use random_quote() in your design to put it in your design.
+ * Set to TRUE to enable.
+ */
+$userand = FALSE;
+
+
+/*
+ * rand_text
+ *
+ * Text to put before the quote in random_quote().
+ */
 $rand_text = "Tilfeldig: ";
 
+
+/*
+ * usestats
+ *
+ * Enable use of the statistics.
+ * Set to TRUE to enable.
+ */
+$usestats = FALSE;
+
+
+/*
+ * usemap
+ *
+ * Seatmap to use.
+ */
 $usemap = $base_path."config/defaultmap.txt";
 
+
+/*
+ * seatsize
+ *
+ * [FIXME]
+ */
 $seatsize = 10;
 
-/* User information and verification */
 
-$min_pass_length = 5;           // (RECOMMENDED > 5)
+/*
+ * min_pass_length
+ *
+ * Minimum length of passwords.
+ * Should be more that five (5).
+ */
+$min_pass_length = 5;
 
 
-$admin['info']['mail'] = "laaknor@globelan.net";
-$admin['info']['nick'] = "Laaknor";
-$admin['info']['name'] = "Lars Åge Kamfjord";
+/*
+ * admin['info']['mail']
+ *
+ * Emailaddress to OSGlobeLAN administrator.
+ * Default is "osglobelan@<server>".
+ */
+$admin['info']['admin'] = "osglobelan@".$_SERVER['SERVER_NAME'];
 
 
-/* What person should recieve webshop-requests */
-$webshop_mail_to = "laaknor@globelan.net";
-/* What do you want to have displayed before the request is sent to the canteen-person? */
-$webshop_display_infoline = "Vennligst vær oppmerksom på:
-<br>- Å ha penger tilgjengelig når en fra kiosk-personalet kommer
-<br>- At steking av pizza ol. kan ta tid.
-<br>- Kioskpersonalet vil ta kontakt med deg via epost dersom de lurer på noe. Ha derfor e-post klienten din tilgjengelig.";
+/*
+ * admin['info]['nick']
+ * 
+ * Nickname of OSGlobeLAN administrator.
+ */
+$admin['info']['nick'] = "OSGlobeLAN";
 
+
+/*
+ * admin['info']['name']
+ *
+ * Name of OSGlobeLAN administrator.
+ */
+$admin['info']['name'] = "OSGlobeLAN Administrator";
+
+
+/*
+ * webshop_mail_to
+ *
+ * Who should recieve webshop-orders.
+ * Default is "osglobelan@<server>".
+ */
+$webshop_mail_to = "osglobelan@".$_SERVER['SERVER_NAME'];
+
+
+/*
+ * webshop_display_infoline
+ *
+ * Information to print before sending the webshop-order.
+ */
+$webshop_display_infoline = "
+									Vennligst vær oppmerksom på:
+										<br>- Å ha penger tilgjengelig når kioskpersonalet kommer.
+										<br>- At steking av pizza o.l. kan ta tid.
+										<br>- Kioskpersonalet tar kontakt via epost dersom de har spørsmål. Sjekk derfor eposten din ofte.
+									";
+
+
+
+/*
+ * Seatsystem
+ *
+ * Do not edit unless you know what you're doing.
+ */
 $seatsign[0]['name'] = "Wall";
 $seatsign[0]['color'] = "black";
 $seatsign[0]['level'] = -2;
@@ -110,6 +258,9 @@ $seatsign[9]['color'] = "red";
 $seatsign[9]['level'] = -2;
 
 
+/*
+ * Do not touch this.
+ */
 for($i=0;$i<count($seatsign);$i++) {
         $sign = $seatsign[$i]['sign'];
 
@@ -118,14 +269,11 @@ for($i=0;$i<count($seatsign);$i++) {
 
 
 
-######################################################
-##
-## NO EDIT!!!
-##
-######################################################
+/*
+ * Do not edit below this!
+ */
 global $base_path;
 $lang_file = "$base_path"."lang/".$language.".php";
-
 
 $lang_inc = require_once $lang_file;
 if(!$lang_inc) die("Could not find language file");
@@ -136,13 +284,12 @@ $styles = NULL;
 $style_folders = $base_path."style/";
 $dir = opendir($style_folders);
 while($read = readdir($dir)) {
-	if($read == "." || $read == ".." || !is_dir($style_folders.$read));
-	else {
-		$styles[] = $read;
-	}
+   if($read == "." || $read == ".." || !is_dir($style_folders.$read));
+   else {
+      $styles[] = $read;
+   }
 
 }
-
 
 if($usestats) require_once 'stats_every.php';
 #if($sql_mode == "upgrade") include_once $base_path.'sql_upgrade.php';
