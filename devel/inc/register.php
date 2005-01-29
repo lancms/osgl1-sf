54c2aae33eb87bb4c5fd67b597cc888ad185b868
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-01-29 22:33:59 +0000

    Fixed $_REQUEST to $_POST, as we do not need to use REQUEST here, a little bit safer. Shouldn't matter, but....

--- devel/inc/register.php
+++ devel/inc/register.php
@@ -14,11 +14,11 @@ if(!isset($action))
 
 if($action == "regme")
 {
-    $username = $_REQUEST["username"];
-    $realname = $_REQUEST["realname"];
-    $email = $_REQUEST["email"];
-    $p1 = $_REQUEST["p1"];
-    $p2 = $_REQUEST["p2"];
+    $username = $_POST["username"];
+    $realname = $_POST["realname"];
+    $email = $_POST["email"];
+    $p1 = $_POST["p1"];
+    $p2 = $_POST["p2"];
     $birthDAY = $_POST['birthDAY'];
     $birthMONTH = $_POST['birthMONTH'];
     $birthYEAR = $_POST['birthYEAR'];
@@ -26,8 +26,8 @@ if($action == "regme")
     $street = $_POST['street'];
     $postNr = $_POST['postNr'];
     $postPlace = $_POST['postPlace'];
-    
-    
+
+
 	$checkmail = mysql_query("SELECT * FROM users WHERE EMail LIKE '$email'");
 
 	$verify = verify($username, $email, $p1, $realname);
@@ -92,9 +92,9 @@ else
     <tr><td><?php echo $form[26]; ?> </td><td><input type=password name=p2></td></tr>
     <tr><td><?php echo $form['74']; ?></td><td><input type=text name=street></td></tr>
     <tr><td><?php echo $form['75']; ?></td><td><input type=text name=postNr size=5><input type=text name=postPlace>
-    <tr><td><?php echo $form['76']; ?></td><td><input type=text name=cellphone></td></tr>	
+    <tr><td><?php echo $form['76']; ?></td><td><input type=text name=cellphone></td></tr>
 	<tr><td><?php echo $form['77']; ?></td><td><select name=birthDAY><option value=0></option>
-	
+
 	<?php
 	for($i=1;$i<32;$i++) echo "<option value=$i>$i</option>\n";
     	echo "</select>";
@@ -106,12 +106,12 @@ else
 		//if($y >= 1980 && $y <= 1980) $selected = "SELECTED";
 		echo "<option value=$y $selected>$y</option>\n";
 	}
-	
+
 	?>
     <tr><td><input type=submit value=" Register "></td></tr>
     </table>
     </form>
-			
+
 	<br><br><b><?php echo $msg['42']; ?></b>
 
     <?php
