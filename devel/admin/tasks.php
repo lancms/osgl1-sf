92425d3d0511a7e7489415b5efd2438a09ea6a34
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-01-30 01:02:51 +0000

    Added possibilety of changing how many percent completed the task is, with logging

--- devel/admin/tasks.php
+++ devel/admin/tasks.php
@@ -16,7 +16,7 @@ if(!isset($action)) {
 		echo "</th><th>".lang("Completed %", "admin_tasks", "Table header, Task Completed %");
 		echo "</th></tr>";
 	}
-	
+
 	while($r = fetch($q)) {
 	echo "<tr><td><a href=admin.php?adminmode=tasks&action=edit&edit=$r->ID>";
 	echo $r->name;
@@ -25,24 +25,24 @@ if(!isset($action)) {
 	echo "</td><td>";
 	echo $r->complete." %";
 	echo "</td></tr>";
-	
+
 	}
-	
+
 	echo "</table><br><br>";
-	
-	
+
+
 	echo "<form method=POST action=admin.php?adminmode=tasks&action=add>";
 	echo "<input type=text name=name> ".lang("Task name", "admin_tasks", "Form-field Task Name");
 	echo "<br><input type=submit value='".lang("Add task", "admin_tasks", "Form submit: add task")."'>";
 	echo "</form>";
-	
+
 }
 
 elseif($action == "add") {
 	$name = $_POST['name'];
 	$exists = query("SELECT * FROM tasks WHERE name = '$name'");
 	if(num($exists) != 0) nicedie(lang("Sorry, but the task has already been added.", "admin_tasks", "Task-error: task already added").refresh("admin.php?adminmode=tasks", 2));
-	
+
 	query("INSERT INTO tasks SET name = '$name'");
 	echo lang("Task added, returning to task index", "admin_tasks", "Task added, returning to index of tasks");
 	refresh("admin.php?adminmode=tasks", 2);
@@ -66,22 +66,34 @@ elseif($action == "edit" && isset($edit)) {
 		if($chuser->ID == getcurrentuserid()) $selected = "SELECTED";
 		echo "<option value=$chuser->ID $selected>$chuser->nick</option>";
 	}
+	echo "</select>";
 	echo "<input type=submit value='".lang("Change assigned user", "admin_tasks", "form-submit to change user in tasks")."'>";
 	echo "</form>";
-			
-	
-	
+
+	echo "<form method=POST action=admin.php?adminmode=tasks&action=changeComplete&edit=$edit>";
+	echo "<select name=complete>";
+	for($i=0;$i<11;$i++) {
+		$display_as = $i."0";
+		if($r->complete == $display_as) $celected = " SELECTED";
+		echo "<option value=$display_as".$celected.">$display_as%</option>";
+	}
+	echo "</select>";
+	echo "<input type=submit value='".lang("Change completeness", "admin_tasks", "form-submit to change completeness")."'>";
+	echo "</form>";
+
+
+
 	$q2 = query("SELECT * FROM tasks_log WHERE taskID = $edit");
-	
+
 	while($r2 = fetch($q2)) {
 		echo "<br><br><hr><br>";
 		echo IDtonick($r2->userID).": ".$r2->logText;
 	}
 	echo "<br><br><form method=POST action=admin.php?adminmode=tasks&action=addcomment&edit=$edit>";
-	echo "<textarea cols=65 rows=10 name=comment>";
+	echo "<textarea cols=65 rows=10 name=comment></textarea>";
 	echo "<br><input type=submit value='".lang("Add comment", "admin_tasks", "Submit-button to add comment on a task")."'>";
 	echo "</form>";
-	
+
 }
 
 elseif($action == "addcomment" && isset($edit)) {
@@ -93,10 +105,20 @@ elseif($action == "addcomment" && isset($edit)) {
 // admin.php?adminmode=tasks&action=changeuser&edit=<taskID>&changeuser=<userID>
 elseif($action == "changeuser" && isset($edit) && isset($_GET['changeuser'])) {
 	$chuser = $_GET['changeuser'];
-	
+
 	query("UPDATE tasks SET userID = $chuser WHERE ID = $edit");
 	query("INSERT INTO tasks_log SET userID = ".getcurrentuserid().", taskID = $edit, logUNIX = ".time().", logText = '".lang("Changed assigned to user to: ", "admin_tasks", "What to put into the SQL-table when a user changes who a task is assigned to").IDtonick($chuser)."'");
-	
+
 	echo lang("Changed assigned user successfully", "admin_tasks", "What to display after we have changed the user a task is assigned to");
+	refresh("admin.php?adminmode=tasks&action=edit&edit=$edit", 2);
+}
+
+elseif($action == "changeComplete" && isset($edit)) {
+	$complete = $_POST['complete'];
+
+	query("UPDATE tasks SET complete = $complete WHERE ID = $edit");
+	query("INSERT INTO tasks_log SET userID = ".getcurrentuserid().", taskID = $edit, logUNIX = ".time().", logText = '".
+		lang("Changed completed percentage to: $complete", "admin_tasks", "Text to insert into the tasks_log-table after the %-completed is changed")."'");
+	echo lang("Changed completeness successfully", "admin_tasks", "Text to display after we have changed the completed-percentage");
 	refresh("admin.php?adminmode=tasks&action=edit&edit=$edit", 2);
 }
