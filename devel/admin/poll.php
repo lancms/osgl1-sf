affe14ebf4a6b9bbfa590bbaa396a91f447e3599
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-01-31 22:28:08 +0000

    nice'ification and mysql_escape_string()ing.

--- devel/admin/poll.php
+++ devel/admin/poll.php
@@ -6,39 +6,29 @@ if(!acl_access("poll"))
 	nicedie($admin[noaccess]);
 
 
-
-if(isset($_GET['action'])) {
-
+if(isset($_GET['action']))
+{
 	$action = $_GET['action'];
-
-} else {
-
+}
+else
+{
 	$action = "list";
-
 }
 
-$poll = $_GET['poll'];
+$poll = mysql_escape_string ($_GET['poll']);
 
 
 
-if($action == "list") {
-
+if($action == "list")
+{
 	echo "<table>";
-
-
 
 	$query = query("SELECT * FROM pollQ");
 
-
-
 	$num = num($query);
 
-
-
-	for($o=0;$o<$num;$o++) {
-
-
-
+	for($o=0;$o<$num;$o++)
+	{
 		$row = fetch($query);
 
 		echo "<tr><td>$row->text</td>";
@@ -49,24 +39,18 @@ if($action == "list") {
 
 		echo "<td><a href=admin.php?adminmode=poll&action=reset&poll=$row->ID>$form[19]</a></td>";
 
-
-
-		if($row->isOpen == 1) $toggle = $form[23];
+		if($row->isOpen == 1)
+		{
+			$toggle = $form[23];
+		}
 
 		else $toggle = $form[22];
 
 		echo "<td><a href=admin.php?adminmode=poll&action=toggle&poll=$row->ID>$toggle</a></td>";
 
-
-
 		echo "</tr>";
-
-
-
 	}
-
-
-
+	
 	echo "</table>";
 
 	?>
@@ -83,8 +67,9 @@ if($action == "list") {
 
 	<?php
 
-} elseif($action == "delete" && isset($poll)) {
-
+}
+elseif(($action == "delete") && (isset($poll)))
+{
 	query("DELETE FROM pollQ WHERE ID = $poll");
 
 	query("DELETE FROM pollA WHERE QID = $poll");
@@ -92,24 +77,22 @@ if($action == "list") {
 	query("DELETE FROM pollVoted WHERE pollID = $poll");
 
 	refresh("admin.php?adminmode=poll", "0");
-
 }
 
-elseif($action == "add") {
+elseif($action == "add")
+{
+	$pollQ = mysql_escape_string ($_POST['pollQ']);
 
-	$pollQ = $_POST['pollQ'];
-
-	$maxVotes = $_POST['maxV'];
+	$maxVotes = mysql_escape_string ($_POST['maxV']);
 
 	$query = query("INSERT INTO pollQ SET text = '$pollQ', maxVotes = $maxVotes");
 
 	refresh("admin.php?adminmode=poll", "0");
 
-} elseif($action == "edit" && isset($poll)) {
-
+}
+elseif($action == "edit" && isset($poll))
+{
 	$queryQ = query("SELECT * FROM pollQ WHERE ID = $poll");
-
-
 
 	$queryA = query("SELECT * FROM pollA WHERE QID = $poll");
 
@@ -117,26 +100,19 @@ elseif($action == "add") {
 
 	echo "<center>$rowQ->text</center>";
 
-	if(num($queryA) == 0) {
-
+	if(num($queryA) == 0)
+	{
 		echo "<br><br>".$form[21];
-
-	} else {
-
-
-
-		for($A=0;$A<num($queryA);$A++) {
-
+	}
+	else
+	{
+		for($A=0;$A<num($queryA);$A++)
+		{
 			$rowA = fetch($queryA);
 
-
-
 			echo "<br>$rowA->Atext";
-
 		}
-
 	}
-
 	?>
 
 	<br><br>
@@ -153,62 +129,48 @@ elseif($action == "add") {
 
 	<?php
 
+}
+elseif(($action == "addanswer") && (isset($poll)))
+{
+	$answer = mysql_escape_string ($_POST['answer']);
 
-
-
-
-} elseif($action == "addanswer" && isset($poll)) {
-
-
-
-	$answer = $_POST['answer'];
-
-	if(!isset($answer)) die($form[38]);
-
+	if(!isset($answer))
+	{
+		die($form[38]);
+	}
+	
 	$query = query("INSERT INTO pollA SET Atext = '$answer', QID = $poll");
 
 	refresh("admin.php?adminmode=poll&action=edit&poll=$poll", "0");
 
-} elseif($action == "toggle" && isset($poll)) {
-
-
-
+}
+elseif(($action == "toggle") && (isset($poll)))
+{
 	$query = query("SELECT isOpen FROM pollQ WHERE ID = $poll");
 
 	$row = fetch($query);
 
-
-
-	if($row->isOpen == 1) {
-
+	if($row->isOpen == 1)
+	{
 		$isOpen = 0;
-
-	} else {
-
-		$isOpen = 1;
-
 	}
-
-
+	else
+	{
+		$isOpen = 1;
+	}
 
 	query("UPDATE pollQ SET isOpen = $isOpen WHERE ID = $poll");
 
-
-
 	refresh("admin.php?adminmode=poll", "0");
 
-} elseif($action == "reset" && isset($poll)) {
-
-
-
+}
+elseif($action == "reset" && isset($poll))
+{
 	query("UPDATE pollA SET votes = 0 WHERE QID = $poll");
 
 	query("DELETE FROM pollVoted WHERE pollID = $poll");
 
 	refresh("admin.php?adminmode=poll", 0);
-
 }
-
-
 
 ?>
