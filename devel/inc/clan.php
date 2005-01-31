17eaa9f62f88d615d2e5720185455754c099a408
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-01-31 23:44:07 +0000

    Cleanup and mysql_escape_string()s.

--- devel/inc/clan.php
+++ devel/inc/clan.php
@@ -4,16 +4,25 @@ if(!config("usepage_compo"))
 	nicedie($msg['1']);
 
 $action = $_GET['action'];
-if(getcurrentuserid() == 1) nicedie(lang("You have to be logged in to view this page!", "inc_clan", "Error-msg to display in inc/clan if you are not logged in"));
+
+if(getcurrentuserid() == 1)
+{
+	nicedie(lang("You have to be logged in to view this page!", "inc_clan", "Error-msg to display in inc/clan if you are not logged in"));
+}
 
 
 if(!isset($action)) {
-	$q = query("SELECT * FROM Clan WHERE moderator = ".getcurrentuserid());
-	if(num($q) == 0) echo $clan[3];
-	else while($r = fetch($q)) {
+	$q = query("SELECT * FROM Clan WHERE moderator = '".mysql_escape_string(getcurrentuserid())."'");
+	if(num($q) == 0)
+	{
+		echo $clan[3];
+	}
+	else while($r = fetch($q))
+	{
 		echo "<br><a href=?inc=clan&action=edit&edit=$r->ID>$r->name</a>";
 	}
-	if(num($q) < 3) {
+	if(num($q) < 3)
+	{
 	?>
 	<br><br><hr><br><form method=POST action=index.php?inc=clan&action=addclan>
 	<?php echo $clan[2]; ?><input type=text name=name>
@@ -26,26 +35,34 @@ if(!isset($action)) {
 
 }
 
-elseif($action == "addclan") {
+elseif($action == "addclan")
+{
 	$name = $_POST['name'];
 	$password = $_POST['password'];
 	$cpass = crypt_pwd($password);
 	$about = addslashes($_POST['about']);
 
 	$test = query("SELECT * FROM Clan WHERE name LIKE '$name'");
-	if(num($test) != 0) die($clan[7]);
-	query("INSERT INTO Clan SET name = '$name',
-			password = '$password',
-			about = '$about',
-			moderator = ".getcurrentuserid());
+	if(num($test) != 0)
+	{
+		die($clan[7]);
+	}
+	query("INSERT INTO Clan SET name = '".mysql_escape_string($name)."',
+			password = '".mysql_escape_string($password)."',
+			about = '".mysql_escape_string($about)."',
+			moderator = '".mysql_escape_string(getcurrentuserid())."'");
 	echo $form[40];
 	echo $clan[6];
 	refresh("index.php?inc=clan", 5);
 }
-elseif($action == "edit" && isset($_GET['edit'])) {
+elseif($action == "edit" && isset($_GET['edit']))
+{
 	$edit = $_GET['edit'];
-	if(!mayEditClan($edit)) die($msg[25]);
-	$q = query("SELECT * FROM Clan WHERE ID = ".$edit);
+	if(!mayEditClan($edit))
+	{
+		die($msg[25]);
+	}
+	$q = query("SELECT * FROM Clan WHERE ID = '".mysql_escape_string($edit)."'");
 	$r = fetch($q);
 
 	echo "<table><form method=POST action=index.php?inc=clan&action=doedit&edit=$r->ID>";
@@ -60,24 +77,33 @@ elseif($action == "edit" && isset($_GET['edit'])) {
 	echo "</form></table>";
 }
 
-elseif($action == "doedit" && isset($_GET['edit'])) {
+elseif($action == "doedit" && isset($_GET['edit']))
+{
 	$edit = $_GET['edit'];
 	if(!mayEditClan($edit))
+	{
 		nicedie($msg['25']);
+	}
 
 	$name = $_POST['name'];
 	$password = $_POST['password'];
 	$about = $_POST['about'];
-	$test = query("SELECT * FROM Clan WHERE name LIKE '$name' AND ID != $edit");
+	$test = query("SELECT * FROM Clan WHERE name LIKE '".mysql_escape_string($name)."' AND ID != '".mysql_escape_string($edit)."'");
 	if(num($test) != 0)
+	{
 		nicedie($clan7);
+	}
 
-	query("UPDATE Clan SET name = '$name',
-			password = '$password',
-			about = '$about'
-			WHERE ID = $edit");
+	query("UPDATE Clan SET name = '".mysql_escape_string($name)."',
+			password = '".mysql_escape_string($password)."',
+			about = '".mysql_escape_string($about)."'
+			WHERE ID = '".mysql_escape_string($edit)."'");
 	echo "$clan[9]";
 	refresh("index.php?inc=clan", 2);
 }
+else
+{
+nicedie();
+}
 
-else nicedie();
+?>
