660f4036dfbefad8ee9b3b3613d44a1a18b8a070
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-01 14:26:42 +0000

    Cleanup.
    Spaceindenters must die().

--- devel/index.php
+++ devel/index.php
@@ -1,33 +1,43 @@
 <?php
 
-require './config/config.php';
+require_once ('./config/config.php');
 
-include $base_path."style/top.php";
-if(isset($_GET['inc'])) {
-	if(strchr($_GET['inc'], "."))
+require_once ($base_path."style/top.php");
+
+if (isset($_GET['inc']))
+{
+	if (strchr($_GET['inc'], "."))
+	{
 		nicedie($msg['29']);
-
-    include "inc/".$_GET['inc'].".php";
-} elseif(isset($_GET['page'])) {
-	$page = mysql_escape_string ($_GET['page']);
-	$query = query("SELECT * FROM static WHERE header LIKE '".$page."'");
+	}
+	require_once ("inc/".$_GET['inc'].".php");
+}
+elseif (isset($_GET['page']))
+{
+	$page = $_GET['page'];
+	$query = query("SELECT * FROM static WHERE header LIKE '".mysql_escape_string($page)."'");
 	$object = fetch($query);
 	if($object->useNL2BR == 1)
+	{
 		echo display_text($object->text);
-	else echo $object->text;
-
-
-} else {
-   $query = query("SELECT * FROM static WHERE header LIKE 'index'");
-   $o = fetch($query);
-   echo display_text($o->text);
-
-    if(config("usepage_news") && $usenews) {
-		echo "<br><br><hr><br>";
-		include 'inc/news.php';
+	}
+	else
+	{
+		echo $object->text;
 	}
 }
-//echo "<br>".lang("This is a test", "index");
+else
+{
+	$query = query("SELECT * FROM static WHERE header LIKE 'index'");
+	$o = fetch($query);
+	echo display_text($o->text);
 
-include $base_path."style/bottom.php";
+	if ((config("usepage_news")) && ($usenews))
+	{
+		echo "<br><br><hr><br>";
+		require_once ('inc/news.php');
+	}
+}
+
+require_once ($base_path."style/bottom.php");
 ?>
