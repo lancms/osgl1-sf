b05465f00187c49cb903fac9524b3650b0d9ab8e
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-01 00:33:18 +0000

    Cleanups and mysql_escape_string()s.
    TODO: Needs an update for the new seatsystem.

--- devel/inc/profile.php
+++ devel/inc/profile.php
@@ -7,16 +7,14 @@ if (!config("usepage_profile"))
 	nicedie($msg[9]);
 }
 
-db_connect();
-
-if(!isset($_GET['action'])) {
-
+if(!isset($_GET['action']))
+{
 	$action = "display";
 
-} else {
-
+}
+else
+{
 	$action = $_GET['action'];
-
 }
 
 
@@ -33,25 +31,20 @@ else
 	$viewUSER = $_GET['uid'];
 }
 
-if($action == "display" && isset($viewUSER)) {
+if (($action == "display") && (isset($viewUSER)))
+{
+	$query = query("SELECT * FROM users WHERE ID = '".mysql_escape_string($viewUSER)."'");
 
-	$query = mysql_query("SELECT * FROM users WHERE ID = $viewUSER") or nicedie(mysql_error());
-
-
-	if(mysql_num_rows($query) == 0) {
+	if(num($query) == 0)
+	{
 		nicedie($form['18']);
 	}
 
-
-
-	$row = mysql_fetch_object($query);
-
-
+	$row = fetch($query);
 
 	$permission = $row->AllowPublic;
 
 	// Check if the user allows others than admins to see his profile.
-
 	if (($permission == 2) && (!acl_access("isAdmin")) && (!acl_access("isChief")) && (!acl_access("adminUsers")))
 	{
 		nicedie($profile['0']);
@@ -62,30 +55,13 @@ if($action == "display" && isset($viewUSER)) {
 	}
 
 	// else: it looks like the user allows even anonymous users to view the profile :)
-
 	echo "<center>".$profile[2].$row->nick."<br><br>";
-
 	echo "<table>";
-
 	profile_table($profile[3], $row->firstName." ".$row->lastName);
-
 	profile_table($profile[4],$row->aboutMe);
-
-
-/*
-	// Needs update to new seat-system.
-
-	echo "<tr><td class=profileLeft>$profile[5]</td><td class=profileRight>";
-
-	if($row->seatX == -1 && $row->seatY == -1) echo $msg[11];
-
-	else echo $msg[10]." ($row->seatX / $row->seatY)";
-
-	echo "</td></tr>";
-*/
-
 	profile_table($profile[9], resolve_groupname($row->ID));
 
+	// TODO: Needs to show seatinfo from the new system.
 }
 
 echo "</table>";
@@ -96,20 +72,6 @@ if(acl_access("adminUsers"))
 }
 
 echo "</center>";
-
-
-/*
-	// This also needs a update for the new seatssytem?
-
-function profile_table($profileLeft, $profileRight) {
-
-	echo "<tr><td class=profileLeft>$profileLeft</td><td class=profileRight>";
-
-	echo $profileRight;
-
-	echo "</td></tr>";
-
+echo "</td></tr>";
 }
-*/
-
 ?>
