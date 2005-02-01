d59b25a9d60219b36db03d816d6b0695f901959f
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-01 00:49:37 +0000

    Cleanups.

--- devel/inc/showusers.php
+++ devel/inc/showusers.php
@@ -1,25 +1,29 @@
 <?php
 
-$query = mysql_query("SELECT userID FROM session WHERE session.userID = 1")
-    or nicedie(mysql_error());
+$query = query("SELECT userID FROM session WHERE session.userID = 1");
 
-$nr = mysql_num_rows($query);
+$nr = num($query);
 
 if($nr == 1)
-    echo "1 guest";
+{
+	echo "1 guest";
+}
 elseif ($nr == 0)
-    echo $msg[16];
+{
+	echo $msg[16];
+}
 else
-    echo "$nr guests";
+{
+	echo "$nr guests";
+}
 
-$query = mysql_query("SELECT DISTINCT users.Nick, users.ID FROM session INNER JOIN users ON users.ID = session.userID WHERE session.userID > 1 ORDER BY users.nick ASC")
-    or nicedie(mysql_error());
+$query = query("SELECT DISTINCT users.Nick, users.ID FROM session INNER JOIN users ON users.ID = session.userID WHERE session.userID > 1 ORDER BY users.nick ASC");
 
-$nr = mysql_num_rows($query);
+$nr = num($query);
 
 for($i=0;$i<$nr;$i++)
 {
-    $d = mysql_fetch_row($query);
+    $d = fetch_array($query);
     echo ",<a href='index.php?inc=profile&uid=$d[1]'>$d[0]</a>";
 }
 
