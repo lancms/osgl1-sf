3b996c7263a0f108e4443d3b57614563eff1238f
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-02 13:41:06 +0000

    Cleanup.

--- devel/style_incs/online_users.php
+++ devel/style_incs/online_users.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'config/config.php';
+
+require_once ('config/config.php');
 
 $query = query("SELECT DISTINCT userID,IP FROM session WHERE userID != 1");
 echo $msg[21];
@@ -9,3 +10,5 @@ $total = query("SELECT DISTINCT userID,IP FROM session WHERE userID = 1");
 echo "<br>";
 echo $msg[22];
 echo num($total);
+
+?>
