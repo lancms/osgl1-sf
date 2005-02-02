64fdd3a8b06ae1f31d86eae0d855455a46a3a1e0
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-02 10:29:52 +0000

    Cleanups. mysql_escape_string()s to escape_string().

--- devel/inc/clan.php
+++ devel/inc/clan.php
@@ -1,27 +1,35 @@
 <?php
-require_once 'config/config.php';
-if(!config("usepage_compo"))
+
+require_once ('config/config.php');
+
+if (!config("usepage_compo"))
+{
 	nicedie($msg['1']);
+}
 
 $action = $_GET['action'];
 
-if(getcurrentuserid() == 1)
+if (getcurrentuserid() == 1)
 {
 	nicedie(lang("You have to be logged in to view this page!", "inc_clan", "Error-msg to display in inc/clan if you are not logged in"));
 }
 
 
-if(!isset($action)) {
-	$q = query("SELECT * FROM Clan WHERE moderator = '".mysql_escape_string(getcurrentuserid())."'");
-	if(num($q) == 0)
+if (!isset($action))
+{
+	$q = query("SELECT * FROM Clan WHERE moderator = '".escape_string(getcurrentuserid())."'");
+	if (num($q) == 0)
 	{
 		echo $clan[3];
 	}
-	else while($r = fetch($q))
+	else
 	{
-		echo "<br><a href=?inc=clan&action=edit&edit=$r->ID>$r->name</a>";
+		while($r = fetch($q))
+		{
+			echo "<br><a href=?inc=clan&action=edit&edit=$r->ID>$r->name</a>";
+		}
 	}
-	if(num($q) < 3)
+	if (num($q) < 3)
 	{
 	?>
 	<br><br><hr><br><form method=POST action=index.php?inc=clan&action=addclan>
@@ -32,9 +40,7 @@ if(!isset($action)) {
 	</form>
 	<?php
 	} // end if(num($q) < 3)
-
 }
-
 elseif($action == "addclan")
 {
 	$name = $_POST['name'];
@@ -42,27 +48,27 @@ elseif($action == "addclan")
 	$cpass = crypt_pwd($password);
 	$about = addslashes($_POST['about']);
 
-	$test = query("SELECT * FROM Clan WHERE name LIKE '$name'");
-	if(num($test) != 0)
+	$test = query("SELECT * FROM Clan WHERE name LIKE '".escape_string($name)."'");
+	if (num($test) != 0)
 	{
-		die($clan[7]);
+		nicedie($clan[7]);
 	}
-	query("INSERT INTO Clan SET name = '".mysql_escape_string($name)."',
-			password = '".mysql_escape_string($password)."',
-			about = '".mysql_escape_string($about)."',
-			moderator = '".mysql_escape_string(getcurrentuserid())."'");
+	query("INSERT INTO Clan SET name = '".escape_string($name)."',
+			password = '".escape_string($password)."',
+			about = '".escape_string($about)."',
+			moderator = '".escape_string(getcurrentuserid())."'");
 	echo $form[40];
 	echo $clan[6];
 	refresh("index.php?inc=clan", 5);
 }
-elseif($action == "edit" && isset($_GET['edit']))
+elseif (($action == "edit") && (isset($_GET['edit'])))
 {
 	$edit = $_GET['edit'];
-	if(!mayEditClan($edit))
+	if (!mayEditClan($edit))
 	{
-		die($msg[25]);
+		nicedie($msg[25]);
 	}
-	$q = query("SELECT * FROM Clan WHERE ID = '".mysql_escape_string($edit)."'");
+	$q = query("SELECT * FROM Clan WHERE ID = '".escape_string($edit)."'");
 	$r = fetch($q);
 
 	echo "<table><form method=POST action=index.php?inc=clan&action=doedit&edit=$r->ID>";
@@ -77,10 +83,10 @@ elseif($action == "edit" && isset($_GET['edit']))
 	echo "</form></table>";
 }
 
-elseif($action == "doedit" && isset($_GET['edit']))
+elseif (($action == "doedit") && (isset($_GET['edit'])))
 {
 	$edit = $_GET['edit'];
-	if(!mayEditClan($edit))
+	if (!mayEditClan($edit))
 	{
 		nicedie($msg['25']);
 	}
@@ -88,16 +94,16 @@ elseif($action == "doedit" && isset($_GET['edit']))
 	$name = $_POST['name'];
 	$password = $_POST['password'];
 	$about = $_POST['about'];
-	$test = query("SELECT * FROM Clan WHERE name LIKE '".mysql_escape_string($name)."' AND ID != '".mysql_escape_string($edit)."'");
-	if(num($test) != 0)
+	$test = query("SELECT * FROM Clan WHERE name LIKE '".escape_string($name)."' AND ID != '".escape_string($edit)."'");
+	if (num($test) != 0)
 	{
 		nicedie($clan7);
 	}
 
-	query("UPDATE Clan SET name = '".mysql_escape_string($name)."',
-			password = '".mysql_escape_string($password)."',
-			about = '".mysql_escape_string($about)."'
-			WHERE ID = '".mysql_escape_string($edit)."'");
+	query("UPDATE Clan SET name = '".escape_string($name)."',
+			password = '".escape_string($password)."',
+			about = '".escape_string($about)."'
+			WHERE ID = '".escape_string($edit)."'");
 	echo "$clan[9]";
 	refresh("index.php?inc=clan", 2);
 }
