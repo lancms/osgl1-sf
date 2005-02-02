b5458a9a0040be8bc334e6ec8c9cde108970ef55
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-02 09:09:57 +0000

    Mailman didn't like my large commitmails, so I'll commit some smaller bulks. :-p
    RFC: aclcheck($acl)-function that checks for access to $acl and nicedie()s if not?

--- devel/admin/userlist.php
+++ devel/admin/userlist.php
@@ -3,9 +3,12 @@ require 'config/config.php';
 
 $action = $_GET['action'];
 
-if($action == "display" || !isset($action)) {
-	if(!acl_access("adminUsers"))
-		nicedie($admin[noaccess]);
+if (($action == "display") || (!isset($action)))
+{
+	if (!acl_access("adminUsers"))
+	{
+		nicedie ($admin['noaccess']);
+	}
 		
 	echo "<table>";
 	$query = query("SELECT * FROM users WHERE ID != 1 ORDER BY ID ASC");
@@ -13,31 +16,32 @@ if($action == "display" || !isset($action)) {
 	$num = num($query);
 	echo "<tr><th>$profile[8]</th>";
 	echo "</tr>";
-	for($i=0;$i<$num;$i++) {
-
+	for($i=0;$i<$num;$i++)
+	{
 		$row = fetch($query);
 
 		echo "<tr><td><a href=index.php?inc=useradmin&user=$row->ID>$row->nick</a></td>";
 		echo "</tr>";
-
 	}
 	echo "</table>";
 }
-
-elseif($action == "online") {
-	if(!acl_access("onlineUsers"))
-		nicedie($admin[noaccess]);
-
+elseif ($action == "online")
+{
+	if (!acl_access("onlineUsers"))
+	{
+		nicedie($admin['noaccess']);
+	}
+	
 	echo "<table>";
 	echo "<tr><th>$profile[8]</th><th>IP</th><th>".$userlist['0']."</th><th>".$userlist['1']."</th></tr>";
 	$query = query("SELECT * FROM session ORDER BY userID");
 	$num = num($query);
 
-	for($i=0;$i<$num;$i++) {
-
+	for($i=0;$i<$num;$i++)
+	{
 		$row = fetch($query);
 
-		$nick = query("SELECT nick FROM users WHERE ID = $row->userID");
+		$nick = query("SELECT nick FROM users WHERE ID = '".escape_string($row->userID)."");
 		$nick = fetch($nick);
 		$nick = $nick->nick;
 
