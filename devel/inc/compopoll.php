cf3d737de8de352018cc27797770866a9a6fa2de
Mathias Bøhn Grytemark <mboehn(at)github.com>
2005-02-02 10:37:48 +0000

    Cleanups. Added FIXMEs for ACLs and lang(). mysql_escape_string()s to escape_string().

--- devel/inc/compopoll.php
+++ devel/inc/compopoll.php
@@ -1,31 +1,36 @@
 <?php
-require_once 'config/config.php';
-if(!config("usepage_compopoll"))
+
+require_once ('config/config.php');
+
+if (!config("usepage_compopoll"))
 {
+	// FIXME: lang()
 	nicedie("Vi kjører ikke noen compoavstemning nå......");
 }
 
 if(getcurrentuserid() == 1)
 {
+	// FIXME: lang() and ACLs.
 	nicedie("Logg inn FØR du prøver å gjøre noe her...");
 }
 
 if(getuserrank() > 0)
 {
+	// FIXME: lang() and ACLs.
 	nicedie("Sorry, men jeg tror vi dropper at crew fyller ut her.... Lak");
 }
 
 $action = $_GET['action'];
 
-if(!isset($action))
+if (!isset($action))
 {
 	echo $msg['41'];
 	echo "<table>";
 	echo "<form method=POST action=index.php?inc=compopoll&action=editvote>";
 	$q = query("SELECT * FROM compoPoll");
-	while($r = fetch($q))
+	while ($r = fetch($q))
 	{
-		$ruser = query("SELECT * FROM compoPollA WHERE pollID = '".mysql_escape_string($r->ID)."' AND userID = '".mysql_escape_string(getcurrentuserid())."'");
+		$ruser = query("SELECT * FROM compoPollA WHERE pollID = '".escape_string($r->ID)."' AND userID = '".escape_string(getcurrentuserid())."'");
 		$r2 = fetch($ruser);
 		echo "<tr><td>";
 		echo $r->question;
@@ -61,17 +66,19 @@ elseif ($action == "editvote")
 	{
 		$question = $r->ID;
 		$answer = $_POST[$question];
-		$test = query("SELECT * FROM compoPollA WHERE userID = '".mysql_escape_string(getcurrentuserid())."' AND pollID = '".mysql_escape_string($question)."'");
+		$test = query("SELECT * FROM compoPollA WHERE userID = '".escape_string(getcurrentuserid())."' AND pollID = '".escape_string($question)."'");
 		if(num($test) == 0)
 		{
-			query("INSERT INTO compoPollA SET pollID = '$question',
-				userID = '".getcurrentuserid()."',
-				answer = '$answer'");
+			query("INSERT INTO compoPollA SET pollID = '".escape_string($question)."',
+				userID = '".escape_string(getcurrentuserid())."',
+				answer = '".escape_string($answer)."'");
 		}
 		else
 		{
-			query("UPDATE compoPollA SET answer = '".mysql_escape_string($answer)."' WHERE pollID = '".mysql_escape_string($question)."' AND userID = '".mysql_escape_string(getcurrentuserid())."'");
+			query("UPDATE compoPollA SET answer = '".escape_string($answer)."' WHERE pollID = '".escape_string($question)."' AND userID = '".escape_string(getcurrentuserid())."'");
 		}
 	} // End while
 refresh("index.php?inc=compopoll", 0);
 } // End action == "editvote"
+
+?>
