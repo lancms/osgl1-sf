8b6010e5d2549f5162e21081b559851c63e30f39
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-03-01 22:43:34 +0000

    At some point, I shoud a) learn to read, b) test something before I put it into the production DB, and c) get to bed

--- devel/admin/wannabemin.php
+++ devel/admin/wannabemin.php
@@ -435,7 +435,7 @@ elseif($action == "DoViewUsers") {
 	if($Type == 1) {
 
 	//$query3		=	"SELECT * FROM wannabeAlt WHERE queID = '$QueID'"; // Removed by Lak; wrong!
-	$query3 	= "SELECT * FROM wannabeAlt WHERE ID = $ans";
+	$query3 	= "SELECT * FROM wannabeAlt WHERE ID = $Ans";
 	$result3 	= 	query($query3);
 
 	$var2		=	fetch($result3);
