6a14ffb44176ea47d79581036ccbe6a0223a4ca0
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-03-04 09:15:21 +0000

    Comments should be a text, not limited to varchar(255)

--- devel/docs/mysql_tables.sql
+++ devel/docs/mysql_tables.sql
@@ -402,7 +402,7 @@ PRIMARY KEY ( `ID` )
 DROP TABLE IF EXISTS wannabeComment;
 CREATE TABLE `wannabeComment` (
 `ID` INT( 10 ) NOT NULL AUTO_INCREMENT ,
-`comment` VARCHAR( 225 ) NOT NULL ,
+`comment` text NOT NULL ,
 `like` smallint(1) NOT NULL default '0',
 `user` INT(10) NOT NULL ,
 `by` INT(10) NOT NULL ,
