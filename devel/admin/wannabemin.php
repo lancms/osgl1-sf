61f30f45bb15e03abe02a20a927ccd1dbbdf2c8c
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-03-05 21:57:49 +0000

    Added a "not decided"-option for admins

--- devel/admin/wannabemin.php
+++ devel/admin/wannabemin.php
@@ -471,6 +471,7 @@ elseif ($action == "DoViewUsers")
 	{
 		$Check2 = " checked";
 	}
+	else $Check0 = " checked";
 
 	$list .= "
 		<form name='AddComment' method='post' action='admin.php?adminmode=wannabemin&action=AddComment'>
@@ -486,6 +487,7 @@ elseif ($action == "DoViewUsers")
 			</tr>
 			<tr>
 				<td><input type='radio' name='approve' value='1'".$Check1.">".lang("Yes", "admin_wannabemin", "Text to display in wannabemin&action=DoViewUsers")."</td>
+				<td><input type='radio' name='approve' value='0'".$Check0.">!".lang("Not decided yet", "admin_wannabemin", "Text to display in wannabemin&action=DoViewUsers")."</td>";
 				<td><input type='radio' name='approve' value='2'".$Check2.">".lang("Nope", "admin_wannabemin", "Text to display in wannabemin&action=DoViewUsers")."</td>
 			</tr>
 			<tr>
