1d6e7fd5a64e08bd2e7e353b7d7600f54b41714f
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-03-12 21:19:55 +0000

    lang()ified and added write_menu("do.php?action=logout") at the bottom

--- devel/style/menu.php
+++ devel/style/menu.php
@@ -12,7 +12,7 @@ include $style."/menu.php";
 
 /* The main page ;) */
 echo "<font class=menu>";
-write_menu("index.php",$title['page'][0]);
+write_menu("index.php",lang("Main Page", "style_menu", "menuitem in write_menu"));
 
 /* Insert the static files; they usually contains info, so they should be the first the user comes to */
 if(config("usepage_static"))  {
@@ -27,36 +27,34 @@ if(config("usepage_static"))  {
 
 /* write out the menu for pages you configure in config.php */
 if(config("usepage_news"))
-    write_menu("index.php?inc=news&many=ALL", $title['page'][9]);
+    write_menu("index.php?inc=news&many=ALL",lang("News", "style_menu", "menuitem in write_menu"));
 
-if(config("usepage_faq")) write_menu("index.php?inc=faq",$title['page'][1]);
-if(config("usepage_register")) write_menu("index.php?inc=register", $title['page'][2]);
-if(config("usepage_poll")) write_menu("index.php?inc=poll", $title['page'][5]);
-if(config("usepage_seat")) write_menu("seat.php", $title['page'][6]);
-if(config("usepage_compo")) write_menu("index.php?inc=compo", $title['page'][8]);
-if(config("usepage_forum")) write_menu("forum.php", $title['page'][12]);
-if(config("usepage_partyweb")) write_menu("partyweb/", $title['page'][16]);
+if(config("usepage_faq")) write_menu("index.php?inc=faq",lang("FAQ", "style_menu", "menuitem in write_menu"));
+if(config("usepage_register")) write_menu("index.php?inc=register",lang("Register", "style_menu", "menuitem in write_menu"));
+if(config("usepage_poll")) write_menu("index.php?inc=poll",lang("Polls", "style_menu", "menuitem in write_menu"));
+if(config("usepage_seat")) write_menu("seat.php", lang("Seatmap", "style_menu", "menuitem in write_menu"));
+if(config("usepage_compo")) write_menu("index.php?inc=compo", lang("Composystem", "style_menu", "menuitem in write_menu"));
+if(config("usepage_forum")) write_menu("forum.php", lang("Forum", "style_menu", "menuitem in write_menu"));
+if(config("usepage_partyweb")) write_menu("partyweb/", lang("Partyweb", "style_menu", "menuitem in write_menu"));
 if(config("usepage_show_stats")) write_menu("index.php?inc=stats", lang("Statistics", "style_menu", "Menuitem for stats"));
 if(getcurrentuserid() == 1) {
 
 }
 else
 {
-    // In this design, we put this one on top :)
-    //write_menu ("do.php?action=logout", $title['page'][3]);
-    write_menu ("index.php?inc=useradmin", $title['page'][7]);
+    write_menu ("index.php?inc=useradmin", lang("Edit profile", "style_menu", "menuitem in write_menu"));
 //    if(config("usepage_kiosk")) write_menu ("index.php?inc=kiosk", $title['page'][15]); // Doesn't work
-    if(config("usepage_compo")) write_menu("index.php?inc=clan", $title['page'][10]);
-    if(config("usepage_compopoll")) write_menu("index.php?inc=compopoll", $title['page'][15]);
-    if(config("usepage_wannabe")) write_menu("index.php?inc=wannabe", $title['page'][17]);
+    if(config("usepage_compo")) write_menu("index.php?inc=clan", lang("Clanregistration", "style_menu", "menuitem in write_menu"));
+    if(config("usepage_compopoll")) write_menu("index.php?inc=compopoll", lang("Compopoll", "style_menu", "menuitem in write_menu"));
+    if(config("usepage_wannabe")) write_menu("index.php?inc=wannabe", lang("WannabeCrew", "style_menu", "menuitem in write_menu"));
 
 
 //    if(getuserrank() >= 1) {
         // This user has crew access! write meny-stuff for crew here!
-if(acl_access("loginUser")) write_menu("index.php?inc=userlogin", $title['page'][13]);
-if(acl_access("isCrew")) write_menu("index.php?inc=adressbook", $title['page'][14]);
-if(acl_access("displayAdmin")) write_menu("admin.php", "ADMIN");
-
+if(acl_access("loginUser")) write_menu("index.php?inc=userlogin", lang("Userlogin", "style_menu", "menuitem in write_menu"));
+if(acl_access("isCrew")) write_menu("index.php?inc=adressbook", lang("Crewaddressbook", "style_menu", "menuitem in write_menu"));
+if(acl_access("displayAdmin")) write_menu("admin.php", lang("ADMIN", "style_menu", "menuitem in write_menu"));
+write_menu("do.php?action=logout", lang("Logout", "style_menu", "menuitem in write_menu"));
 #		write_menu("http://crew.globelan.net", "Crewforum");
 //    } if(getuserrank() == 2) {
         // *wheeeee* admin-access!
