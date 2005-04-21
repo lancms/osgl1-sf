035f167a3fed40184bcafe87db47929184cbe408
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2005-04-21 21:17:03 +0000

    Changed the way userlogin happens. Now possible for admins/crew to add "comments" on users (I.E. if it is "problems" with information the user has given)

--- devel/inc/userlogin.php
+++ devel/inc/userlogin.php
@@ -8,7 +8,7 @@ if(!acl_access("loginUser"))
 $action = $_GET['action'];
 $user = $_GET['user'];
 $ticketarray = NULL;
-$ticketarray .= "</select>";
+$ticketarray .= "</select>"; // FIXME: WTF is this?
 
 if (!isset($action))
 {
@@ -71,7 +71,7 @@ elseif ($action == "main")
 		}
 		else
 		{
-			echo "<td bgcolor=red><a href=index.php?inc=userlogin&action=logout&user=$row->ID>$form[42]</a>";
+			echo "<td bgcolor=red><a href=index.php?inc=userlogin&action=login&user=$row->ID>$form[42]</a>";
 		}
 		echo "</td><td>";
 		echo $row->name;
@@ -93,16 +93,54 @@ elseif ($action == "main")
 	}
 echo "</table>";
 }
-elseif ($action == "login")
+
+elseif ($action == "login") {
+	$escUser = escape_string($user);
+	$q = query("SELECT * FROM users WHERE ID = ".$escUser);
+	$r = fetch($q);
+	
+	echo "<table>";
+	osgl_table("<a href=index.php?inc=userlogin>".lang("Back to searchpage", "inc_userlogin", "link to go back to userlogin main")."</a>","");
+	osgl_table(lang("Users First name", "inc_userlogin", "Field to show in userlogin for first name"), $r->firstName);
+	osgl_table(lang("Users Last name", "inc_userlogin", "Field to show in userlogin for last name"), $r->lastName);
+	osgl_table(lang("Users birthday/birthmonth/birthyear", "inc_userlogin", "Field to show in userlogin for birthday"), $r->birthDAY."/".$r->birthMONTH."/".$r->birthYEAR);
+	osgl_table(lang("Users email", "inc_userlogin", "Field to show in userlogin for email"), $r->EMail);
+	osgl_table(lang("Users Postaddress (number place)", "inc_userlogin", "Field to show in userlogin for postaddress"), $r->postNr." ".$r->postPlace);
+	osgl_table(lang("Users seatreservation", "inc_userlogin", "Field to show in userlogin for first name"), $r->seatX." / ".$r->seatY);
+	
+	if($r->isHere == 0) osgl_table("", "<a href=?inc=userlogin&action=doLogin&user=$user>".lang("Mark as arrived", "inc_userlogin", "link to mark user as arrived")."</a>");
+	else osgl_table("", "<a href=?inc=userlogin&action=doLogout&user=$user>".lang("Mark as departed", "inc_userlogin", "link to mark user as departed")."</a>");
+	
+	echo "<form method=POST action=index.php?inc=userlogin&action=loginComments&user=$user>";
+	osgl_table(lang("Crewcomments", "inc_userlogin", "textarea for admins/crew to put in their comments about the user in userlogin"), "<textarea name=comments cols=65 rows=10>$r->loginComments</textarea>");
+	
+	osgl_table("", "<input type=submit value='".lang("Save comments", "inc_userlogin", "submit-button to save comments")."'>");
+	echo "</form>";
+	echo "</table>";
+
+
+}
+
+elseif($action == "loginComments") {
+	$escUser = escape_string($user); 
+	$comment = $_POST['comments'];
+	
+	query("UPDATE users SET loginComments = '".escape_string($comment)."' WHERE ID = $escUser");
+	dblog(14, $user.":::".$comment);
+	refresh("index.php?inc=userlogin&action=login&user=$user", 0);
+	
+	
+}
+elseif ($action == "doLogin")
 {
 	$query = query("UPDATE users SET isHere = 1 WHERE ID = '".escape_string($user)."'");
-	refresh("index.php?inc=userlogin", 0);
+	refresh("index.php?inc=userlogin&action=login&user=$user", 0);
 	dblog(12, "User arrived");
 }
-elseif($action == "logout")
+elseif($action == "doLogout")
 {
 	$query = query("UPDATE users SET isHere = 0 WHERE ID = '".escape_string($user)."'");
-	refresh("index.php?inc=userlogin", 0);
+	refresh("index.php?inc=userlogin&action=login&user=$user", 0);
 	dblog(13, "User departed");
 }
 ?>
