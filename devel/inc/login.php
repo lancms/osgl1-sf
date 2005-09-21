83d084c53c126e5a24b7c6a98ea06f3a617b74fe
Aleksander Lengard <wacie(at)globelan(dot)net>
2005-09-21 16:29:27 +0000

    I have'nt tested this, but i think it sould work. The only thing that does not work is the mail() function.

--- devel/inc/login.php
+++ devel/inc/login.php
@@ -5,7 +5,7 @@ $action = $_GET['action'];
 
 if ($q == 1)
 {
-    ?>
+?>
     <form method=post action=do.php?action=login>
     <br>
     <input type=text name=username size=10>
@@ -14,7 +14,7 @@ if ($q == 1)
     <br>
     <input type=submit value='<?php echo $form[0]; ?>'>
     </form>
-    <?php
+<?php
 }
 
 if($action == "resendPwd") {
@@ -72,7 +72,9 @@ elseif ($action == "doResendPwd") {
           $query  = "UPDATE users SET lastPasswordReset = '" . time() . "' WHERE EMail = '" . $mail . "'";
           $result = query($query);
           
-          $query  = "UPDATE users SET tempPassword = '" . $tempPwd . "' WHERE EMail = '" . $mail . "'";
+          $md5Pwd = md5($tempPwd);
+          
+          $query  = "UPDATE users SET tempPassword = '" . $md5Pwd . "' WHERE EMail = '" . $mail . "'";
           $result = query($query);
           
           /* FIX THIS */
@@ -106,6 +108,94 @@ elseif ($action == "doResendPwd") {
     echo lang("You need to fill inn your email adress" , "inc_login" , "No-Mail text");
     refresh("?inc=login&action=resendPwd" , "2");
     
+  }
+
+}
+
+elseif($action == "verifyPwd") {
+ 
+  echo "
+  <form method='post' action='?inc=login&action=doVerifyPwd'>
+    " . lang("Forgotten your password?" , "inc_login" , "Forgotten password text") . " <br>
+    " . lang("Your mail adress:" , "inc_login" , "Mail adress text") . " <input type='text' name='email'> <br>
+    " . lang("Username:" , "inc_login" , "Username text") . " <input type='text' name='username'> <br>
+    " . lang("Temp password:" , "inc_login" , "Temp Password text") . " <input type='text' name='tempPwd'> <br>
+    <input type='submit' value='" . lang("Verify" , "inc_login" , "Submit button text") . "'>
+  </form>
+  ";
+  
+}
+
+elseif($action == "doVerifyPwd") {
+
+  $email    = $_POST['email'];
+  $tempPwd  = $_POST['tempPwd'];
+  $username = $_POST['username'];
+  
+  if(empty($email) || empty($tempPwd) || empty($username)) {
+    
+    echo lang("You need to fill out all the fields" , "inc_login" , "Empty form");
+    refresh("?inc=login&action=verifyPwd" , "2");
+    
+  } else {
+  
+    $query  = "SELECT * FROM users WHERE EMail = '" . $email . "'";
+    $result = query($query);
+    
+    $i      = fetch($result);
+    
+    $dbUser = $i->nick;
+    $dbTempPwd = md5($i->tempPwd);
+    
+    if($dbUser == $username) {
+    
+      if($tempPwd == $dbTempPwd) {
+      
+        echo lang("Success, please change your password. Ane PLEASE remember it this time" , "inc_login" , "Success text") . " <br>
+        <form method='post' action='?inc=login&action=doChangePwd'>
+          " . lang("New Password:" , "inc_login" , "Password text") . " <input type='text' name='pwd1'> <br>
+          " . lang("Repeat Password" , "inc_login" , "Password text") . " <input type='text' name='pwd2'> <br>
+          <input type='submit' value='" . lang("Change" , "inc_login" , "Submit button text") . "'>
+        </form>
+        ";
+      
+      } else {
+      
+        echo lang("The temp password does not match the temp password in the mail" , "inc_login" , "Temp passwords does not match");
+        refresh("?inc=login&action=verifyPwd" , "2");
+      
+      }
+    
+    } else {
+    
+      echo lang("The username does not match the email adress" , "inc_login" , "username does not match email");
+      refresh("?inc=login&action=verifyPwd");
+    
+    }
+  
+  }
+
+}
+
+elseif($action == "doChangePwd") {
+
+  $pwd1   = $_POST['pwd1'];
+  $pwd2   = $_POST['pwd2'];
+  $MD5Pwd = MD5($pwd1);
+  
+  if($pwd1 == $pwd2) {
+    
+    $query  = "UPDATE users SET password = '" . $md5Pwd . "' WHERE EMail = '" . $email . "'";
+    $result = query($query);
+    
+    echo lang("Your password has now been changed, you can now login" , "inc_login" , "Success changed");
+    refresh("?inc=login" , "2");
+  
+  } else {
+    
+    echo lang("The passwords does not match" , "inc_login" , "Not match");
+    refresh("?inc=login&action=verifyPwd" , "2");
+  
   }
 
 }
