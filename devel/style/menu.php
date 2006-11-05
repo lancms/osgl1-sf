828cdcd701fc0859bf209ed4d56497f3e04dd40b
Lars Åge Kamfjord <lars(dot)age(at)kamfjord(dot)org>
2006-11-05 22:20:26 +0000

    I probably should reinstall easyPHP on my computer so that I could test my commits before I commit....

--- devel/style/menu.php
+++ devel/style/menu.php
@@ -16,7 +16,7 @@ write_menu("index.php",lang("Main Page", "style_menu", "menuitem in write_menu")
 
 /* Insert the static files; they usually contains info, so they should be the first the user comes to */
 if(config("usepage_static"))  {
-	if(acl_access("isCrew)) $staticFiles = '1,0';
+	if(acl_access("isCrew")) $staticFiles = '1,0';
 	else $staticFiles = '0';
     $q = mysql_query("SELECT * FROM static WHERE header != 'index' AND showPage = 1 AND crewOnly IN ($staticFiles)") or die(mysql_error());
     while($r = mysql_fetch_object($q)) {
