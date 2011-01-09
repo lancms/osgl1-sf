0a53a5c611f51ebdbcdb70b772be5b3d2b945b76
Mathias Bøhn Grytemark <mboehn(at)github.com>
2011-01-09 11:49:23 +0000

    Updated htdocs.

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,21 +11,22 @@
 
 <p><b>The original description:</b> A LAN-party organizing system. Has seatregistration, composignup, polls, support for multiple designs and much more. The system is primarily used by <a class='link' href='http://www.globelan.net'>GlobeLAN</a>, but we try to make it as useable as possible for other parties too.</p>
 
+
 <p><b>Current version: </b>
-<br />Development of the 1.0 codebase have been halted, only serious security issues will be fixed.
-<br />We have started OSGL2 (OSGlobeLAN2), but we don't have a release or fully working featureset yet.
+<br />We have released some alphas of OSGL2. GlobeLAN is currently using the svn trunk for it's website.
 <ul>
-	<li>Version 0.9: Available on <a class='link' href='http://sourceforge.net/project/showfiles.php?group_id=77474'>SourceForge</a>.</li>
-	<li>Nightly snapshot of the 1.0 codebase: Available <a class='link' href='http://download.globelan.net/devel_snapshot.tar.bz2'>here</a>.</li>
-	<li>SVN readonly for OSGL2: Available on <a class='link' href='http://sourceforge.net/svn/?group_id=77474'>SourceForge</a>.</li>
+	<li>Latest OSGL2 release on <a href='https://sourceforge.net/projects/osglobelan/files/osglobelan-tng/'>SourceForge</a>
+	<li>SVN available on <a class='link' href='http://sourceforge.net/svn/?group_id=77474'>SourceForge</a></li>
 </ul>
 </p>
 
+<p><b>Old versions: </b>
+<br />We no longer support the old releases called "devel" or "osgl1". We've not touched this code in a long time, and it's considered obsolete.
+</p>
+
+
 <p><b>Getting started: </b>
-<ul>
-	<li>1.0 codebase: Read the guide in docs/INSTALL.</li>
-	<li>OSGL2: Well, don't. But if you do, check the documentation in docs/</li>
-</ul>
+<br />The documentation available for OSGL2 is placed in the docs/ folder inside the release.
 </p>
 
 <p><b>Developers:</b> Lars &Aring;ge "Laaknor" Kamfjord is the main developer and the guy with most of the ideas. He's supported by Mathias "Alfa" B&oslash;hn Grytemark and from time to time other people from within the GlobeLAN-community. If you would like to help, send us an <a class='link' href='http://sourceforge.net/mail/?group_id=77474'>email</a>.</p>
